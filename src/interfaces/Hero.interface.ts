ce3df229920c961c645d747b8e0f7b40b6a6c49a
Oribel Fonseca <oribelfonseca@gmail.com>
2025-07-29 19:22:32 -0400

    Create Hero.interface.ts

---
+++ src/interfaces/Hero.interface.ts
@@ -0,0 +1,11 @@
+export interface Hero {
+    id:                 string;
+    superhero:          string;
+    publisher:          string;
+    alter_ego:          string;
+    first_appearance:   string;
+    characters:         string;
+    strong:             number;
+    speed:              number;
+    inteligense:        number;
+}
