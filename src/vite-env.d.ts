d162d8e4648092d6ffd07b1cd3560d4c5eb3b401
Oribel Fonseca <oribelfonseca@gmail.com>
2025-08-08 20:27:33 -0400

    Update vite-env.d.ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,14 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+    readonly VITE_API_BASE_URL: string
+    readonly VITE_ENV: 'development' | 'production'
+    // readonly VITE_TOKEN_NAME: string
+    readonly VITE_LOGIN_ENDPOINT: string
+    readonly VITE_LOGOUT_ENDPOINT: string
+    readonly VITE_USER_ENDPOINT: string
+}
+  
+interface ImportMeta {
+    readonly env: ImportMetaEnv
+}
