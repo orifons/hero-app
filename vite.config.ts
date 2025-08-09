a38b0bd74d9dddfb3c739330e1e2bf59b4a3c05a
Oribel Fonseca <oribelfonseca@gmail.com>
2025-08-08 20:27:31 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,30 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
-export default defineConfig({
-    plugins: [react(), tailwindcss()],
+// // https://vite.dev/config/
+// export default defineConfig({
+//     plugins: [react(), tailwindcss()],
+//     define: {
+//         'process.env': {},
+//         __APP_ENV__: JSON.stringify(mode),
+//       },
+//     resolve: {
+//         alias: {
+//             "@": path.resolve(__dirname, "./src"),
+//         },
+//     },
+// });
 
+
+export default defineConfig(({ mode }) => ({
+    plugins: [react(), tailwindcss()],
+    define: {
+        'process.env': {},
+        __APP_ENV__: JSON.stringify(mode),
+      },
     resolve: {
         alias: {
             "@": path.resolve(__dirname, "./src"),
         },
     },
-});
+  }))
