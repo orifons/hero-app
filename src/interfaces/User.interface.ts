0b49ff585eb4eb55ac159d727da8d86a1f144b8c
Oribel Fonseca <oribelfonseca@gmail.com>
2025-08-12 13:22:58 -0400

    Update User.interface.ts

--- src/interfaces/User.interface.ts
+++ src/interfaces/User.interface.ts
@@ -1,19 +1,27 @@
+// export interface User {
+//   id?: number;
+//   last_login?: string;
+//   is_superuser: boolean;
+//   username: string;
+//   first_name: string;
+//   last_name: string;
+//   email?: string;
+//   is_staff: boolean;
+//   is_active: boolean;
+//   date_joined: string;
+//   phone_number: string;
+//   address?: string;
+//   city?: string;
+//   country?: string;
+//   avatar?: string;
+//   groups?: string[];
+//   user_permissions?: string[];
+// }
+
 export interface User {
-    id?: number,
-    last_login?: string,
-    is_superuser: boolean,
-    username: string,
-    first_name: string,
-    last_name: string,
-    email?: string,
-    is_staff: boolean,
-    is_active: boolean,
-    date_joined: string,
-    phone_number: string,
-    address?: string,
-    city?: string,
-    country?: string,
-    avatar?: string,
-    groups?: string[],
-    user_permissions?: string[]
+  _id?: string;
+  username: string;
+  email: string;
+  createdAt?: Date;
+  name: string;
 }
